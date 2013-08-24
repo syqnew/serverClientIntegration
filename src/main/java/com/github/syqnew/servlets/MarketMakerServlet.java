eb051ffe7e17a4a99b7f04035218e8729e455b88
snew <syqnew@mit.edu>
2013-08-24 02:26:47 -0400

    Added the servlet mappings for Quote and MarketMaker.

--- src/main/java/com/github/syqnew/servlets/MarketMakerServlet.java
+++ src/main/java/com/github/syqnew/servlets/MarketMakerServlet.java
@@ -1,7 +1,25 @@
 package com.github.syqnew.servlets;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MarketMakerServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+	}
+
 
 }
