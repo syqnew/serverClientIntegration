40faf1201b17a03e7bef35bb00f01ef3088ac859
snew <syqnew@mit.edu>
2013-08-24 23:26:53 -0400

    Changed the News table to exclude time.

--- src/main/java/com/github/syqnew/domain/News.java
+++ src/main/java/com/github/syqnew/domain/News.java
@@ -11,7 +11,6 @@ public class News implements BaseObject {
 	@GeneratedValue
 	private int id;
 	private String message;
-	private long time;
 
 	public News() {
 	}
@@ -34,14 +33,6 @@ public class News implements BaseObject {
 
 	public void setMessage(String message) {
 		this.message = message;
-	}
-
-	public long getTime() {
-		return time;
-	}
-
-	public void setTime(long time) {
-		this.time = time;
 	}
 
 }
